df8f562f03995d718dc77d9f34e2a2a3b7ce7b32
jakob-he <jakob.hertzberg@googlemail.com>
2021-09-08 13:51:40 +0200

    Rearranged the repository structure to fit current Pypi release
    - Predicting and annotating of single variant files is now support with the default option
    - The data directory is now included in the package to allow for predicting with pypi release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tadacnv',
-      version='1.0.0',
+      version='1.0.1',
       description='Annotation of TADs and CNVs',
       long_description='This package allows to annotate CNVs with annotations expressing their functional impact. Based on the functional annotation, a classifier can be trained and used to prioritze CNVs.',
       classifiers=[
